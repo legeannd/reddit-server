0a2b1e48985bc40b88d64c44b31c436bfb015a16
Gean Lucas <gean.lfpsa@gmail.com>
2020-09-24 21:37:19 -0300

    Added cors

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,12 @@ import { buildSchema } from 'type-graphql';
 import redis from 'redis';
 import session from 'express-session';
 import connectRedis from 'connect-redis';
+import cors from 'cors';
 import microConfig from './mikro-orm.config';
 import { HelloResolver } from './resolvers/hello';
 import { PostResolver } from './resolvers/post';
 import { UserResolver } from './resolvers/user';
 import { __prod__ } from './constants';
-import { MyContext } from './types';
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
@@ -21,6 +21,12 @@ const main = async () => {
 
   const RedisStore = connectRedis(session);
   const redisClient = redis.createClient();
+  app.use(
+    cors({
+      origin: 'http://localhost:3000',
+      credentials: true,
+    }),
+  );
 
   app.use(
     session({
@@ -43,10 +49,13 @@ const main = async () => {
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: ({ req, res }: MyContext): MyContext => ({ em: orm.em, req, res }),
+    context: ({ req, res }) => ({ em: orm.em, req, res }),
   });
 
-  apolloServer.applyMiddleware({ app });
+  apolloServer.applyMiddleware({
+    app,
+    cors: false,
+  });
 
   app.listen(4000, () => {
     console.log('server started on localhost:4000');
@@ -60,5 +69,3 @@ const main = async () => {
 };
 
 main();
-
-console.log('hello orld');
