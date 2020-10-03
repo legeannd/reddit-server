538f49ada391fb9c2f10b9223bbe2e4c42eb26e3
Gean Lucas <gean.lfpsa@gmail.com>
2020-10-03 20:31:50 -0300

    Disabling migrations from running on every restart

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ import { createUserLoader } from './utils/createUserLoader';
 import { createUpdootLoader } from './utils/createUpdootLoader';
 
 const main = async () => {
-  const conn = await createConnection({
+  /* const conn =  */ await createConnection({
     type: 'postgres',
     url: process.env.DATABASE_URL,
     logging: true,
@@ -28,7 +28,7 @@ const main = async () => {
     migrations: [path.join(__dirname, './migrations/*')],
     entities: [Post, User, Updoot],
   });
-  await conn.runMigrations();
+  // await conn.runMigrations();
 
   /* await Post.delete({}); */
 
